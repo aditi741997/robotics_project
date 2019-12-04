d515153cb5ccef93bda2b0cebdcb4ece40c644e8
Ubuntu <ubuntu@ip-172-31-5-200.ec2.internal>
2019-12-04 23:46:51 +0000

    Adding dummy expt stuff

---
+++ src/beginner_tutorials/src/publisher.cpp
@@ -0,0 +1,188 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <std_msgs/Header.h>
+
+#include <sstream>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+
+/* inline std::string convert_to_str(double d, int len)
+{
+    std::string ans = std::to_string(d);
+    return ans.append(len - ans.length(), ' ');
+} */
+
+void sieve(int64_t limit)
+{
+    int64_t segment_size = (int64_t) std::sqrt(limit);
+    int64_t count = (limit < 2) ? 0 : 1;
+
+    int64_t i = 3;
+    int64_t n = 3;
+    int64_t s = 3;
+
+    std::vector<char> sieve(segment_size);
+    std::vector<char> is_prime(segment_size + 1, true);
+    std::vector<int64_t> primes;
+    std::vector<int64_t> multiples;
+
+    for (int64_t low = 0; low <= limit; low += segment_size)
+    {
+        std::fill(sieve.begin(), sieve.end(), true);
+
+        //current segment : low - high
+        int64_t high = low + segment_size - 1;
+        high = std::min(high, limit);
+
+        // simple sieve
+        for (; i*i < high; i += 2)
+            if (is_prime[i])
+                for (int64_t j = i*i; j <= segment_size; j += i)
+                    is_prime[j] = false;
+
+        // initialize sieving primes for segmented sieve
+        for (; s * s <= high; s += 2)
+        {
+            if (is_prime[s])
+            {
+                primes.push_back(s);
+                multiples.push_back(s * s - low);
+            }
+        }
+
+        for (std::size_t i = 0; i < primes.size(); i++)
+        {
+            int64_t j = multiples[i];
+            for (int64_t k = primes[i] * 2; j < segment_size; j += k)
+                sieve[j] = false;
+            multiples[i] = j - segment_size;
+        }
+
+        for (; n <= high; n += 2)
+            if (sieve[n - low]) // n is a prime
+                count++;
+    }
+
+    std::cout << count << " primes found!" << std::endl;
+}
+
+void calc_primes(int64_t limit)
+{
+    int i, num = 1, primes = 0;
+
+    while (num <= limit) { 
+        i = 2; 
+        while (i <= num) { 
+            if(num % i == 0)
+                break;
+            i++; 
+        }
+        if (i == num)
+            primes++;
+        num++;
+    }
+}
+
+void execute(int64_t limit)
+{
+    // sieve(100);
+    calc_primes(limit);
+}
+
+int main (int argc, char **argv)
+{
+    if (argc < 5)
+    {
+        std::cout << "Not enough params. Need msg size (in bytes), publisher queue length, loop rate, #msgs to send";
+        return 0;
+    }
+
+    int msg_size = atoi(argv[1]);
+    int pub_queue_len = atoi(argv[2]);
+    int ros_rate = atoi(argv[3]);
+    int num_msgs = atoi(argv[4]);
+    int64_t limit = atoi(argv[5]);
+
+    ros::init(argc, argv, "talker");
+
+    ros::NodeHandle n;
+
+    // ros::Publisher chatter_pub = n.advertise<nw_experiments::StringTime>("chatter", pub_queue_len);
+    ros::Publisher chatter_pub = n.advertise<std_msgs::Header>("chatter", pub_queue_len);
+
+    ros::Rate loop_rate(ros_rate);
+
+    // std_msgs::String msg;
+    // nw_experiments::StringTime msgTime;
+    std_msgs::Header hdr;
+
+    std::stringstream ss;
+    char* message;
+    message = new char[msg_size - 4];
+    memset(message, '0', msg_size - 4);
+    message[msg_size - 4 - 1] = '\0';
+    ss << message;
+
+    // This showed that the method to convert to string takes almost 0.001 sec, which is of the order of our latency.
+    // Hence, we can't use this.
+    // ROS_INFO("B4");
+    // msg.data = convert_to_str(11.45678, 256);
+    // ROS_INFO("A8");
+    // msgTime.data = ss.str();
+
+    hdr.frame_id = ss.str();
+
+    while (0 == chatter_pub.getNumSubscribers())
+    {
+        ROS_INFO("Waiting for subscribers to connect");
+        ros::Duration(0.1).sleep();
+    }
+    ros::Duration(0.9).sleep();
+    // Code to check ros::Time format :
+    //ros::Time x = ros::Time::now();
+    // ROS_INFO("%f %f %f", x.sec, x.nsec, x.toSec());
+
+    int i = 0;
+
+    // if we decide to store the values in the future :
+    // std::vector<double> sent_times (num_msgs);
+
+    double total_c1 = 0.0;
+    std::vector<double> c1_arr;
+
+    while (ros::ok() && i < num_msgs)
+    {
+        // msgTime.time_stamp = ros::Time::now();
+        // sent_times[i] = ros::Time::now().toSec();
+        // chatter_pub.publish(msgTime);
+
+        hdr.seq = i;
+        hdr.stamp = ros::Time::now();
+        
+        // TODO : Add a fixed time compute here!!
+        ros::Time c1_start = ros::Time::now();
+        execute(limit);
+	    ros::Time c1_end = ros::Time::now();
+        double x = (c1_end - c1_start).toSec();
+        total_c1 += x; 
+	    c1_arr.push_back(x);
+
+        // std_msgs::String msg;
+        // chatter_pub.publish(msg);
+        chatter_pub.publish(hdr);
+        ros::spinOnce();
+
+        loop_rate.sleep();
+        i += 1;
+    }
+    ROS_INFO("Total msgs sent %i, msg size %i, avg c1 : %f", i, msg_size, total_c1/i);
+    std::sort(c1_arr.begin(), c1_arr.end());
+    ROS_INFO("Mean_Median pub_c1 : median : %f, 99p : %f", c1_arr[c1_arr.size()/2], c1_arr[(99*(c1_arr.size()))/100]);
+    // ROS_INFO(" %f %f %f", sent_times[0], sent_times[num_msgs/2], sent_times[num_msgs -1]);
+    if (ros::ok())
+    {
+        ros::shutdown();
+    }
+    return 0;
+}
---
+++ src/beginner_tutorials/src/subscriber.cpp
@@ -0,0 +1,324 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Header.h"
+#include <fstream>
+#include <cmath>
+#include <sys/sysinfo.h>
+#include <vector>
+#include <algorithm>
+
+// For adding a heavy thread :
+// #include <thread>
+#include <cstdlib>
+#include <stdint.h>
+
+double sum_latency = 0.0;
+double sum_latency_sq = 0.0;
+int msg_count = 0;
+
+int msg_size = 0;
+int pub_queue_len = 0;
+int ros_rate = 0;
+int num_msgs = 0;
+int sub_queue_len = 0;
+int transport_type = 0;
+
+int sub_id = 0;
+bool do_heavy = false;
+
+const double megabyte = 1024*1024;
+double used_ram = 0.0;
+
+ros::Time last_recv_time;
+double sum_recv_delta = 0.0;
+std::vector<double> recv_delta_arr;
+
+bool write_lat = false;
+std::vector<double> latencies;
+
+double sum_total_delay = 0.0;
+std::vector<double> total_delay;
+
+// for calculating tput : keeping array of times b/w outputs of subscriber [i.e. after heavy is over]
+double last_sub_output = 0.0;
+double sum_op_delta = 0.0;
+std::vector<double> op_delta;
+
+// for d+1/t : separate array
+double sum_dt_sum = 0.0;
+std::vector<double> dt_sum;
+
+double avg_heavy_time = 0.0;
+std::vector<double> heavy_times;
+
+int percentile = 99;
+
+int64_t limit;
+bool stop_thread = false;
+
+
+
+void sieve(int64_t limit)
+{
+    int64_t segment_size = (int64_t) std::sqrt(limit);
+    int64_t count = (limit < 2) ? 0 : 1;
+
+    int64_t i = 3;
+    int64_t n = 3;
+    int64_t s = 3;
+
+    std::vector<char> sieve(segment_size);
+    std::vector<char> is_prime(segment_size + 1, true);
+    std::vector<int64_t> primes;
+    std::vector<int64_t> multiples;
+
+    for (int64_t low = 0; low <= limit; low += segment_size)
+    {
+        std::fill(sieve.begin(), sieve.end(), true);
+
+        //current segment : low - high
+        int64_t high = low + segment_size - 1;
+        high = std::min(high, limit);
+
+        // simple sieve
+        for (; i*i < high; i += 2)
+            if (is_prime[i])
+                for (int64_t j = i*i; j <= segment_size; j += i)
+                    is_prime[j] = false;
+
+        // initialize sieving primes for segmented sieve
+        for (; s * s <= high; s += 2)
+        {
+            if (is_prime[s])
+            {
+                primes.push_back(s);
+                multiples.push_back(s * s - low);
+            }
+        }
+
+        for (std::size_t i = 0; i < primes.size(); i++)
+        {
+            int64_t j = multiples[i];
+            for (int64_t k = primes[i] * 2; j < segment_size; j += k)
+                sieve[j] = false;
+            multiples[i] = j - segment_size;
+        }
+
+        for (; n <= high; n += 2)
+            if (sieve[n - low]) // n is a prime
+                count++;
+    }
+
+    std::cout << count << " primes found!" << std::endl;
+}
+
+void calc_primes(int64_t limit)
+{
+    int i, num = 1, primes = 0;
+
+    while (num <= limit) { 
+        i = 2; 
+        while (i <= num) { 
+            if(num % i == 0)
+                break;
+            i++; 
+        }
+        if (i == num)
+            primes++;
+        num++;
+    }
+    ROS_INFO("Found %i primes", primes);
+}
+
+void chatterCallBack(const std_msgs::Header::ConstPtr& msg)
+{
+    ros::Time recv_time = ros::Time::now();
+
+    double recv_delta = (msg_count == 0) ? 0.0 : (recv_time - last_recv_time).toSec();
+    double lat = (recv_time - msg->stamp).toSec();
+    recv_delta_arr.push_back(recv_delta);
+    latencies.push_back(lat);
+    ROS_INFO("sent time : [%f], recv_time : [%f], msg count : %i, msg id : %i", msg->stamp.toSec(), recv_time.toSec(), msg_count, msg->seq);
+
+    msg_count += 1;
+    sum_latency += lat;
+    sum_latency_sq += lat * lat;
+    sum_recv_delta += recv_delta;
+
+    last_recv_time = recv_time;
+
+    // struct sysinfo si;
+    // sysinfo(&si);
+    // used_ram += (si.totalram - si.freeram)/megabyte;
+
+    if (do_heavy)
+    {
+        ROS_INFO("Starting heavy");
+        ros::Time heavy_start = ros::Time::now();
+        // Single heavy function :
+        // sieve(limit);
+        calc_primes(limit);
+
+        // Adding 2 threads :
+        // std::thread t1(sieve, limit);
+        // std::thread t2(sieve, limit);
+        // t1.join();
+        // t2.join();
+
+        ROS_INFO("Ending heavy");
+        avg_heavy_time += (ros::Time::now() - heavy_start).toSec();
+        heavy_times.push_back((ros::Time::now() - heavy_start).toSec());
+    }
+    
+    double dt = 0.0;
+    ros::Time to = ros::Time::now();
+
+    double delay = (to - msg->stamp).toSec();
+    total_delay.push_back(delay);
+    sum_total_delay += delay;
+    dt += delay;
+
+    if (last_sub_output != 0.0)
+    {
+        double op_del = (to).toSec() - last_sub_output;
+        op_delta.push_back(op_del);
+        sum_op_delta += op_del;
+        dt += op_del;
+
+        // dt is the d+1/t for this cycle. Add to array, sum value.
+        sum_dt_sum += dt;
+        dt_sum.push_back(dt);
+    }
+    last_sub_output = to.toSec();
+
+    ROS_INFO("Measuring tput : lso : %f, dt : %f, delay : %f", last_sub_output, dt, delay);
+
+    if (msg->seq >= ((num_msgs*98)/100))
+    {
+        int last_recv_msg = msg->seq;
+        ROS_INFO("Last received msg Id %i", last_recv_msg);
+        
+        double std_dev = sqrt(sum_latency_sq - (sum_latency*sum_latency/msg_count))/msg_count;
+        sum_latency = sum_latency/msg_count;
+        
+        sum_recv_delta /= (msg_count - 1);
+        // sort the arr
+        std::sort(recv_delta_arr.begin(), recv_delta_arr.end());
+        double median_recv_delta = recv_delta_arr[msg_count/2];
+
+        std::sort(latencies.begin(), latencies.end());
+        int index = (percentile*(msg_count))/100;
+        // std::cout << "index : " << index << "\n";
+        double percentile_lat = latencies[index];
+        double median_lat = latencies[latencies.size()/2];
+
+        std::sort(total_delay.begin(), total_delay.end());
+        double perc_total_delay = total_delay[index];
+        double median_total_delay = total_delay[total_delay.size()/2];
+        double mean_total_delay = sum_total_delay/(total_delay.size());
+
+        std::sort(op_delta.begin(), op_delta.end());
+        index = (percentile*(op_delta.size()))/100;
+        double perc_op_delta = op_delta[index];
+        double median_op_delta = op_delta[op_delta.size()/2];
+        double mean_op_delta = sum_op_delta/(op_delta.size());
+
+        std::sort(dt_sum.begin(), dt_sum.end());
+        double perc_dt_sum = dt_sum[index];
+        double median_dt_sum = dt_sum[dt_sum.size()/2];
+        double mean_dt_sum = sum_dt_sum/(dt_sum.size());
+
+        std::sort(heavy_times.begin(), heavy_times.end());
+
+        // used_ram /= msg_count;std::to_string(sub_id) + 
+        avg_heavy_time /= msg_count;
+
+        ROS_INFO("Boo");
+        std::ofstream outfile;
+        outfile.open("1p1s_2cpu_unpinned_subL_dec4.txt", std::ios_base::app);
+        outfile << msg_size << ", " << pub_queue_len << ", " << num_msgs << ", " << sub_queue_len << ", " << ros_rate << ", " << transport_type << ", " << do_heavy << ", " << perc_total_delay << ", " << median_total_delay << ", " << mean_total_delay << ", " << perc_op_delta << ", " << median_op_delta << ", " << mean_op_delta << ", " << perc_dt_sum << ", " << median_dt_sum << ", " << mean_dt_sum << ", " << avg_heavy_time << ", " << "lost_msgs, " << (msg->seq + 1 - msg_count) << ", " << (msg->seq + 1) << ", c1n_latency, " << percentile_lat << ", " << median_lat << ", " << sum_latency << ", " << avg_heavy_time << ", \n"; 
+        // outfile << msg_size << ", " << pub_queue_len << ", " << num_msgs << ", " << sub_queue_len << ", " << ros_rate << ", " << transport_type << ", " << do_heavy << ", " << sum_latency << ", " << percentile_lat << ", " << std_dev << ", " << sum_recv_delta << ", " << median_recv_delta << ", " << avg_heavy_time  << ", " << msg_count << ", " << last_recv_msg << ", \n";
+
+        if (write_lat)
+        {
+            std::ofstream outfile;
+            outfile.open("latencies.txt", std::ios_base::app);
+            outfile << msg_size << ", " << pub_queue_len << ", " << num_msgs << ", " << sub_queue_len << ", " << ros_rate << ", " << transport_type << ", " << do_heavy << "\n";
+            for (double &d: latencies)
+            {
+                outfile << d << "\n";
+            }
+        }
+
+        ROS_INFO("ALL PARAMS : %i, %i, %i, %i, %i, %i, %i, %i", msg_size, pub_queue_len, ros_rate, num_msgs, sub_queue_len, transport_type, sub_id, do_heavy);
+        ROS_INFO("FINAL VALUE OF LATENCY : [%f], STD DEV : [%f], used ram : [%f], sum rec delta : [%f]", sum_latency, std_dev, used_ram, sum_recv_delta);
+        ros::shutdown();
+    }
+}
+
+void thread_func(int64_t limit)
+{
+    std::cout << "Starting thread_func" << std::endl;
+    int loop_count = 0;
+
+    while (!stop_thread)
+    {
+        loop_count += 1;
+        std::cout << loop_count << " RUNS" << std::endl;
+    }
+
+}
+
+int main (int argc, char **argv)
+{
+    if (argc != 10)
+    {
+        std::cout << "Not enough inputs. Need msg size, pub queue len, ros rate, num msgs, sub queue len, transport type (0 tcp, 1 tcpNoDelay, 2 udp), sub id, do heavy, limit for sieve";
+        return 0;
+    }
+
+    msg_size = atoi(argv[1]);
+    pub_queue_len = atoi(argv[2]);
+    ros_rate = atoi(argv[3]);
+    num_msgs = atoi(argv[4]);
+    sub_queue_len = atoi(argv[5]);
+    transport_type = atoi(argv[6]);
+    sub_id = atoi(argv[7]);
+    do_heavy = atoi(argv[8]);
+
+    limit = atoi(argv[9]);
+
+    ros::init(argc, argv, "listener");
+    // ros::init(argc, argv, "listener");
+    ros::NodeHandle n;
+
+    // std::cout << "queue len : " << sub_queue_len << msg_size << pub_queue_len << ros_rate << num_msgs << transport_type << sub_id;
+
+    /* ros::TransportHints transport_type_ros;
+    switch (transport_type)
+    {
+        case 0:
+            transport_type_ros = ros::TransportHints().tcp();
+            std::cout << "t 0\n";
+            break;
+        case 1:
+            transport_type_ros = ros::TransportHints().tcpNoDelay();
+            std::cout << "t 1\n";
+            break;
+        case 2:
+            transport_type_ros = ros::TransportHints().udp();
+            std::cout << "t 2\n";
+            break;
+    } */
+
+    // creating a thread
+    // std::thread t1(thread_func, limit);
+
+    ros::Subscriber sub = n.subscribe("chatter", sub_queue_len, chatterCallBack);
+    std::cout << "chatter subscribed";
+    ros::spin();
+
+    stop_thread = true;
+    // t1.join();
+    return 0;
+}
