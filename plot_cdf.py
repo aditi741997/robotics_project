8e2b0e5a51563a416bbb934c43729bfb4709b9b4
Ubuntu <ubuntu@ip-172-31-66-152.ec2.internal>
2020-06-01 22:59:02 +0000

    Added abs deg metric to plot cdf

--- plot_cdf.py
+++ plot_cdf.py
@@ -2,6 +2,7 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import math
 
 folder = sys.argv[1]
 pre1 = sys.argv[2]
@@ -9,9 +10,33 @@ pre2 = sys.argv[3]
 t = int(sys.argv[4])
 f=15
 
+def read_abs_deg_metric(fname):
+	c=3
+	a = []
+	with open(fname, 'r') as fm:
+        	fml = fm.readlines()
+	        for i in range((len(fml)/c)):
+        		hpp = c*i
+       			try:
+                		#human y coordinate
+                		# hy = float(fml[hpp+2].split(' ')[-1][:-1])
+				hy = float(fml[hpp].split(' ')[-2])
+				tc = float(fml[hpp+2].split(' ')[-2])
+				ts = float(fml[hpp+1].split(' ')[-2])					
+				ty = ts*float((3.0*2*tc)/((2*tc*tc) - 1.0))
+				h_theta = math.atan(hy/3.0)
+				t_theta1 = math.atan(ty/3.0)
+				a.append(t_theta1-h_theta)
+			except:
+				print "error", i, hy, tc, ts, ty
+	b = a[len(a)/100:]
+	print "Length : ", len(a), "Ignoring " , len(a)/100, "Len b : ", len(b)
+	return b
+
+
 sty = ['g.:', 'b*-', 'r^--']
 #need to merge all 5 runs
-for metr in ['RxnTime', 'RelMetric1', 'RelMetric']:
+for metr in ['RxnTime', 'RelMetric1', 'RelMetric', 'Abs Deg']:
 	i = 0
 	for alg in ['RTC', 'Dyn', 'Default']:
 		arr = []
@@ -42,10 +67,17 @@ for metr in ['RxnTime', 'RelMetric1', 'RelMetric']:
 					for l in fil.readlines():
 						if metr in l:
 							arr += [float(x) for x in l.split(', ')[1:-1]]
+				# NEED abs deg metric. Read perf file, ignore first 1%.
+				if 'Abs' in metr:
+					pf = '%s/%s/%s_%s_%s_perf_%i.%i.%i.out'%(folder, alg, pre1, alg, pre2, r,f,t)
+					if i == 1:
+						pf = '%s_%s_%s_New_perf_%i.%i.%i.out'%(pre1, alg, pre2, r,f,t)
+					print pf
+					arr += read_abs_deg_metric(pf)
 		s_arr = np.sort(arr)
 		pr = 1. * np.arange(len(arr))/(len(arr) - 1)
 		print len(pr), len(s_arr)
-		new_l = ((9*len(s_arr))/10)
+		new_l = ((9.8*len(s_arr))/10)
 		s_arr = s_arr[new_l:]
 		pr = pr[new_l:]
 		print len(pr), len(s_arr)
