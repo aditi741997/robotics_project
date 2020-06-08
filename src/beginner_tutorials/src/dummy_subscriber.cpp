4df621df42e9acc8eff72b21f11d5fe492a29568
Ubuntu <ubuntu@ip-172-31-77-175.ec2.internal>
2020-06-08 07:49:49 +0000

    DAG expt setup, RTC running, testing fractional

---
+++ src/beginner_tutorials/src/dummy_subscriber.cpp
@@ -0,0 +1,356 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Header.h"
+#include "sensor_msgs/Image.h"
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <sys/sysinfo.h>
+#include <vector>
+#include <algorithm>
+#include <time.h>
+#include <bits/stdc++.h> 
+#include <sys/time.h>
+#include <stdlib.h>
+// For adding a heavy thread :
+// #include <thread>
+#include <cstdlib>
+#include <stdint.h>
+
+double sum_latency = 0.0;
+double sum_latency_sq = 0.0;
+int msg_count = 0;
+
+int msg_size = 0;
+int pub_queue_len = 0;
+float ros_rate = 0.0;
+int num_msgs = 0;
+int sub_queue_len = 0;
+int transport_type = 0;
+
+int sub_id = 0;
+bool do_heavy = false;
+
+const double megabyte = 1024*1024;
+double used_ram = 0.0;
+
+ros::Time last_recv_time;
+double sum_recv_delta = 0.0;
+std::vector<double> recv_delta_arr;
+
+bool write_lat = false;
+std::vector<double> latencies;
+
+double sum_total_delay = 0.0;
+std::vector<double> total_delay;
+
+// for calculating tput : keeping array of times b/w outputs of subscriber [i.e. after heavy is over]
+double last_sub_output = 0.0;
+double sum_op_delta = 0.0;
+std::vector<double> op_delta;
+
+// for d+1/t : separate array
+double sum_dt_sum = 0.0;
+std::vector<double> dt_sum;
+
+double avg_heavy_time = 0.0;
+std::vector<double> heavy_times;
+
+double compute_rt_sum = 0.0;
+std::vector<double> compute_rt_arr;
+
+double compute_ts_sum = 0.0;
+std::vector<double> compute_ts_arr;
+
+int percentile = 95;
+
+int64_t limit;
+bool stop_thread = false;
+
+bool to_publish = false;
+std::string expt;
+std::string node_name;
+std::string publish_topic = "";
+ros::Publisher chatter_pub;
+std_msgs::Header hdr;
+
+double pub_time = 0.0;
+std::vector<double> pub_times;
+
+double td_latency_sum = 0.0;
+std::vector<double> td_latency_arr;
+
+bool add_rand_noise = false;
+
+void calc_primes(int64_t limit, bool duh)
+{
+    int i, num = 1, primes = 0;
+
+    while (num <= limit) { 
+        i = 2; 
+        while (i <= num) { 
+            if(num % i == 0)
+                break;
+            i++; 
+        }
+        if (i == num)
+            primes++;
+        num++;
+    }
+    if (duh)
+        ROS_INFO("Found %i primes", primes);
+}
+
+void print_smt(double m_sum, std::vector<double> m_arr, std::string m)
+    {
+        int l = m_arr.size();
+        if (l > 0)
+        {
+            std::sort(m_arr.begin(), m_arr.end());
+            double avg = m_sum/l;
+            double med = m_arr[l/2];
+            double perc = m_arr[(l*percentile)/100];
+            ROS_INFO("Mean, median, tail of %s is %f %f %f # Len : %i #", m.c_str(), avg, med, perc, l);            
+        }
+    }
+
+void chatterCallBack(const std_msgs::Header::ConstPtr& msg)
+{
+    ros::Time recv_time = ros::Time::now();
+    clock_t cb_start_rt = clock();
+
+    double recv_delta = (msg_count == 0) ? 0.0 : (recv_time - last_recv_time).toSec();
+    double lat = (recv_time - msg->stamp).toSec();
+    recv_delta_arr.push_back(recv_delta);
+    latencies.push_back(lat);
+    if (msg_count%200 == 5)
+    	ROS_INFO("sent time : [%f], recv_time : [%f], msg count : %i, msg id : %i", msg->stamp.toSec(), recv_time.toSec(), msg_count, msg->seq);
+
+    msg_count += 1;
+    sum_latency += lat;
+    sum_latency_sq += lat * lat;
+    sum_recv_delta += recv_delta;
+
+    last_recv_time = recv_time;
+
+    // struct sysinfo si;
+    // sysinfo(&si);
+    // used_ram += (si.totalram - si.freeram)/megabyte;
+
+    if (do_heavy)
+    {
+        ros::Time heavy_start = ros::Time::now();
+        // Single heavy function :
+        // sieve(limit);
+        calc_primes(limit, true);
+
+        // Adding 2 threads :
+        // std::thread t1(sieve, limit);
+        // std::thread t2(sieve, limit);
+        // t1.join();
+        // t2.join();
+	
+	double compute_rt = (double)(clock() - cb_start_rt)/CLOCKS_PER_SEC;
+    	compute_rt_sum += compute_rt;
+    	compute_rt_arr.push_back(compute_rt);
+
+        avg_heavy_time += (ros::Time::now() - heavy_start).toSec();
+        heavy_times.push_back((ros::Time::now() - heavy_start).toSec());
+    }
+    
+    double dt = 0.0;
+    ros::Time to = ros::Time::now();
+
+    double delay = (to - msg->stamp).toSec();
+    total_delay.push_back(delay);
+    sum_total_delay += delay;
+
+    if (msg_count >= 2)
+    {
+        double op_del = (to).toSec() - last_sub_output;
+        op_delta.push_back(op_del);
+        sum_op_delta += op_del;
+
+        // dt is the d+1/t for this cycle. Add to array, sum value.
+        // dt += op_del + delay;
+        // sum_dt_sum += dt;
+        // dt_sum.push_back(dt);
+
+    	dt = op_del + total_delay[total_delay.size() - 2];
+    	sum_dt_sum += dt;
+    	dt_sum.push_back(dt);
+    }
+    last_sub_output = to.toSec();
+
+    if (to_publish)
+    {
+	ros::Time pub_start = ros::Time::now();
+	// ROS_INFO("About to publish!");
+	hdr.seq = msg->seq;
+	hdr.stamp = msg->stamp;
+	hdr.frame_id = msg->frame_id;
+	chatter_pub.publish(hdr);
+	double pub_t = (ros::Time::now() - pub_start).toSec();
+	pub_time += pub_t;
+	pub_times.push_back(pub_t);
+	if (pub_t > 0.004)
+		ROS_INFO("Subscriber: pub time more than 4ms");
+    }
+
+    if (msg_count%80 == 72)
+    {
+	print_smt(sum_latency, latencies, "Latency at " + node_name);
+	print_smt(compute_rt_sum, compute_rt_arr, "RT Compute time at " + node_name);
+	print_smt(avg_heavy_time, heavy_times, "Ros::Time Compute time at " + node_name);
+    }
+
+    if (msg->seq >= ((num_msgs*98)/100))
+    {
+        int last_recv_msg = msg->seq;
+        ROS_INFO("Last received msg Id %i", last_recv_msg);
+        
+        double std_dev = sqrt(sum_latency_sq - (sum_latency*sum_latency/msg_count))/msg_count;
+        sum_latency = sum_latency/msg_count;
+        
+        sum_recv_delta /= (msg_count - 1);
+        // sort the arr
+        std::sort(recv_delta_arr.begin(), recv_delta_arr.end());
+        double median_recv_delta = recv_delta_arr[msg_count/2];
+
+        std::sort(latencies.begin(), latencies.end());
+        int index = (percentile*(msg_count))/100;
+        // std::cout << "index : " << index << "\n";
+        double percentile_lat = latencies[index];
+        double median_lat = latencies[latencies.size()/2];
+
+        std::sort(total_delay.begin(), total_delay.end());
+        double perc_total_delay = total_delay[index];
+        double median_total_delay = total_delay[total_delay.size()/2];
+        double mean_total_delay = sum_total_delay/(total_delay.size());
+
+        std::sort(op_delta.begin(), op_delta.end());
+        index = (percentile*(op_delta.size()))/100;
+        double perc_op_delta = op_delta[index];
+        double median_op_delta = op_delta[op_delta.size()/2];
+        double mean_op_delta = sum_op_delta/(op_delta.size());
+
+        std::sort(dt_sum.begin(), dt_sum.end());
+        double perc_dt_sum = dt_sum[index];
+        double median_dt_sum = dt_sum[dt_sum.size()/2];
+        double mean_dt_sum = sum_dt_sum/(dt_sum.size());
+
+        std::sort(heavy_times.begin(), heavy_times.end());
+	double perc_heavy = heavy_times[index];
+	double median_heavy = heavy_times[heavy_times.size()/2];
+
+	print_smt(compute_rt_sum, compute_rt_arr, "Real time compute time");
+
+	if (to_publish)
+	{
+		std::sort(pub_times.begin(), pub_times.end());
+	}
+
+        // used_ram /= msg_count;std::to_string(sub_id) + 
+        avg_heavy_time /= msg_count;
+
+	// compute RT :
+	std::sort(compute_rt_arr.begin(), compute_rt_arr.end());
+	int num_crt = compute_rt_arr.size();
+	double perc_ci_rt = compute_rt_arr[(percentile*num_crt)/100];
+	double med_ci_rt = compute_rt_arr[num_crt/2];
+	double mean_ci_rt = compute_rt_sum/num_crt;
+
+        std::ofstream outfile;
+        outfile.open((expt + "_" + node_name + "_May.txt").c_str(), std::ios_base::app);
+        outfile << msg_size << ", " << pub_queue_len << ", " << num_msgs << ", " << sub_queue_len << ", " << ros_rate << ", " << transport_type << ", " << do_heavy << ", " << perc_total_delay << ", " << median_total_delay << ", " << mean_total_delay << ", " << perc_op_delta << ", " << median_op_delta << ", " << mean_op_delta << ", rxnTime, " << perc_dt_sum << ", " << median_dt_sum << ", " << mean_dt_sum << ", " << "lost_msgs, " << (msg->seq + 1 - msg_count) << ", " << (msg->seq + 1) << ", c1n_latency, " << percentile_lat << ", " << median_lat << ", " << sum_latency << ", c2, " << perc_heavy << ", " << median_heavy << ", " << avg_heavy_time << ", c2 rt, " << perc_ci_rt << ", " << med_ci_rt << ", " << mean_ci_rt << ", ";
+	if (to_publish)
+	{
+		outfile << "pub_time, " << pub_times[index] << ", " << pub_times[pub_times.size()/2] << ", " << pub_time/msg_count << ", ";
+	}
+	outfile << "\n";
+        // outfile << msg_size << ", " << pub_queue_len << ", " << num_msgs << ", " << sub_queue_len << ", " << ros_rate << ", " << transport_type << ", " << do_heavy << ", " << sum_latency << ", " << percentile_lat << ", " << std_dev << ", " << sum_recv_delta << ", " << median_recv_delta << ", " << avg_heavy_time  << ", " << msg_count << ", " << last_recv_msg << ", \n";
+
+        if (write_lat)
+        {
+            std::ofstream outfile;
+            outfile.open("latencies.txt", std::ios_base::app);
+            outfile << msg_size << ", " << pub_queue_len << ", " << num_msgs << ", " << sub_queue_len << ", " << ros_rate << ", " << transport_type << ", " << do_heavy << "\n";
+            for (double &d: latencies)
+            {
+                outfile << d << "\n";
+            }
+        }
+
+        ROS_INFO("ALL PARAMS : %i, %i, %i, %i, %i, %i, %i, %i", msg_size, pub_queue_len, ros_rate, num_msgs, sub_queue_len, transport_type, sub_id, do_heavy);
+        ROS_INFO("FINAL VALUE OF LATENCY : [%f], STD DEV : [%f], used ram : [%f], sum rec delta : [%f]", sum_latency, std_dev, used_ram, sum_recv_delta);
+        ros::shutdown();
+    }
+}
+
+int main (int argc, char **argv)
+{
+    if (argc < 13)
+    {
+        std::cout << "Not enough inputs. Need msg size, pub queue len, ros rate, num msgs, sub queue len, transport type (0 tcp, 1 tcpNoDelay, 2 udp), sub id, do heavy, limit for sieve";
+        return 0;
+    }
+
+    msg_size = atoi(argv[1]);
+    pub_queue_len = atoi(argv[2]);
+    ros_rate = atoi(argv[3]);
+    num_msgs = atoi(argv[4]);
+    sub_queue_len = atoi(argv[5]);
+    transport_type = atoi(argv[6]);
+    // sub_id = atoi(argv[7]);
+    do_heavy = atoi(argv[7]);
+
+    limit = atoi(argv[8]);
+    node_name = argv[9];
+
+    ROS_INFO("Init node %s, sub_topic %s, expt %s, num_msgs : %i, ros_rate : %i", node_name.c_str(), argv[10], argv[11], num_msgs, ros_rate);
+    ROS_INFO("Other params : pql %i, sql %i, doheavy %i, limit %i", pub_queue_len, sub_queue_len, do_heavy, limit);
+    ros::init(argc, argv, node_name);
+    ros::NodeHandle n;
+    
+    std::string sub_topic = argv[10];
+    expt = argv[11];
+
+    to_publish = atoi(argv[12]) == 1;
+    if (to_publish)
+    {
+     	publish_topic = argv[13];
+    	chatter_pub = n.advertise<std_msgs::Header>(publish_topic, sub_queue_len);
+    	 std::stringstream ss;
+             char* message;
+             message = new char[msg_size - 4];
+             memset(message, '0', msg_size - 4);
+             message[msg_size - 4 - 1] = '\0';
+             ss << message;
+    	 hdr.frame_id = ss.str();
+
+     /*  publish_topic = "/camera1/rgb/image_raw";
+        chatter_pub = n.advertise<sensor_msgs::Image>(publish_topic, sub_queue_len);
+*/
+
+    	ROS_INFO("I will publish on %s", (publish_topic + "BLAH").c_str());
+
+    	while (0 == chatter_pub.getNumSubscribers())
+    	{
+            ROS_INFO("Subscriber name %s Waiting for subscribers to connect", node_name.c_str());
+            ros::Duration(0.1).sleep();
+        }
+        ros::Duration(1.5).sleep();
+    }
+
+     // creating a thread
+    // std::thread t1(thread_func, limit);
+    
+  // ros::Subscriber sub = n.subscribe("/camera/rgb/image_raw", 1, chatterImgCallBack, ros::TransportHints().tcpNoDelay(), true);
+      ros::Subscriber sub = n.subscribe(sub_topic, sub_queue_len, chatterCallBack, ros::TransportHints().tcpNoDelay());
+    std::cout << "chatter subscribed, about to call spin \n";
+    ros::spin();
+
+    stop_thread = true;
+    // t1.join();
+    return 0;
+}
---
+++ src/beginner_tutorials/src/local_planner.cpp
@@ -0,0 +1,273 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Header.h"
+#include <sstream>
+#include <time.h>
+#include <bits/stdc++.h>
+#include <sys/times.h>
+
+// subscribe to odom and Lcmp and GPlan.
+// measure rxn time w.r.t. all
+// cb (odom) only updates the shared variable and hence does nothing.
+// E.D. w.r.t. GPlan if RTC and w.r.t. LCmp if Dyn.
+// TODO : See if we need to make separate queues for all callbacks.
+class LocalPlanner
+{
+    double last_lcmp_ts, last_gplan_ts, last_odom_ts;
+
+    double compute_rt_sum, compute_ros_sum;
+    std::vector<double> compute_rt_arr, compute_ros_arr;
+
+    double lat_lcmp_sum, lat_gplan_sum, lat_odom_sum;
+    std::vector<double> lat_lcmp_arr, lat_gplan_arr, lat_odom_arr;
+
+    double ts_last_gplan_used, ts_last_odom_used, ts_last_lcmp_used; // TS of the globalPlan used by the last exec of LPlan. 
+    double last_lcmp_out_ts, last_gplan_out_ts, last_odom_out_ts; // Note when was the last time LPlan got a new output w.r.t LCMp/GPlan/Odom.
+
+    // If current LPlan is using new GPlan or odom, update tput
+    double tput_lcmp_sum, tput_gplan_sum, tput_odom_sum;
+    std::vector<double> tput_lcmp_arr, tput_gplan_arr, tput_odom_arr;
+
+    // rxnTm w.r.t. 3 chains :
+    double rxntm_lcmp_sum, rxntm_gplan_sum, rxntm_odom_sum;
+    std::vector<double> rxntm_lcmp_arr, rxntm_gplan_arr, rxntm_odom_arr;
+
+    int climit; // for compute time
+    bool rtc; // rtc vs fractional
+
+    int total_exec_count, total_odom_cb_count, total_gplan_cb_count, total_lcmp_cb_count;
+
+    ros::NodeHandle nh;
+    ros::Publisher cmd_vel_pub;
+    ros::Subscriber lcmp_sub, odom_sub, gplan_sub;
+
+    int perc7 = 75;
+    int percentile = 95;
+    int perc0 = 90;
+    int perc2 = 99;
+    float perc3 = 99.9;
+    float perc4 = 99.99; 
+
+public:
+    LocalPlanner(int lim, bool algo, std::string lcmp_t, std::string odom_t, std::string gplan_t)
+    {
+        climit = lim;
+        rtc = algo;
+
+        ts_last_gplan_used = -1.0;
+        ts_last_odom_used = -1.0;
+        ts_last_lcmp_used = -1.0;
+
+        last_lcmp_ts = -1.0;
+        last_gplan_ts = -1.0;
+        last_odom_ts = -1.0;
+
+        total_exec_count = 0;
+        total_odom_cb_count = 0;
+        total_gplan_cb_count = 0;
+        total_lcmp_cb_count = 0;
+
+	last_odom_out_ts = 0.0;
+	last_gplan_out_ts = 0.0;
+	last_lcmp_out_ts = 0.0;
+
+        // Subscribe to all 3!
+        lcmp_sub = nh.subscribe(lcmp_t, 1, &LocalPlanner::lcmpCallback, this, ros::TransportHints().tcpNoDelay(), true);
+        odom_sub = nh.subscribe(odom_t, 1, &LocalPlanner::odomCallback, this, ros::TransportHints().tcpNoDelay());
+        gplan_sub = nh.subscribe(gplan_t, 1, &LocalPlanner::gplanCallback, this, ros::TransportHints().tcpNoDelay(), true);
+        
+    }
+
+    void odomCallback(const std_msgs::Header::ConstPtr& msg)
+    {
+        // Will need lock if different callbacks have different queues
+        // Default : One single cb queue hence no need to lock.
+        last_odom_ts = msg->stamp.toSec();
+        total_odom_cb_count += 1;
+        // DO nothing!
+    }
+
+    void gplanCallback(const std_msgs::Header::ConstPtr& msg)
+    {
+        // Will need lock if different callbacks have different queues
+        // Default : One single cb queue hence no need to lock.
+        last_gplan_ts = msg->stamp.toSec();
+        total_gplan_cb_count += 1;
+        if (rtc)
+        {
+            // RTC Means LPlan must be E.D. w.r.t. GPlan.
+            exec();
+        }
+    }
+
+    void calcPrimes()
+    {
+        int i, num = 1, primes = 0;
+
+        while (num <= climit) { 
+            i = 2; 
+            while (i <= num) { 
+                if(num % i == 0)
+                    break;
+                i++; 
+            }
+            if (i == num)
+                primes++;
+            num++;
+        }
+
+        ROS_INFO("Found %i primes", primes);
+    }
+
+    void print_smt(double m_sum, std::vector<double> m_arr, std::string m)
+    {
+        int l = m_arr.size();
+        if (l > 0)
+        {
+            std::sort(m_arr.begin(), m_arr.end());
+            double avg = m_sum/l;
+            double med = m_arr[l/2];
+            double perc = m_arr[(l*percentile)/100];
+        
+            ROS_INFO("Median, %i tail of %s is %f %f , arr sz %i , %i : %f, %i : %f, %i : %f, %f : %f, %f : %f #", percentile, m.c_str(), med, perc, l, perc7, m_arr[(l*perc7)/100], perc0, m_arr[(l*perc0)/100], perc2, m_arr[(l*perc2)/100], perc3, m_arr[(l*perc3)/100], perc4, m_arr[(l*perc4)/100]);            
+        }
+    }
+
+    void exec()
+    {
+        total_exec_count += 1;
+
+        double ros_start = ros::Time::now().toSec();
+        clock_t rt_start = clock();
+        // This LPlan cb will use the gplan, odom, lcmp with these TS:
+        double using_gplan_ts = last_gplan_ts;
+        double using_odom_ts = last_odom_ts;
+        double using_lcmp_ts = last_lcmp_ts;
+
+        double lat_lcmp = (ros::Time::now()).toSec() - using_lcmp_ts;
+        double lat_gplan = (ros::Time::now()).toSec() - using_gplan_ts;
+        double lat_odom = ros::Time::now().toSec() - using_odom_ts;
+
+        calcPrimes();
+        // TODO : publish cmd_vel.
+
+        if (using_gplan_ts > ts_last_gplan_used)
+        {
+            // Using a new GPLan : Add lat, tput rxnTm LCMP
+            lat_gplan_sum += lat_gplan;
+            lat_gplan_arr.push_back(lat_gplan);
+
+            if (last_gplan_out_ts > 0.0)
+            {
+                double tput_gplan = ros::Time::now().toSec() - last_gplan_out_ts;
+                // ROS_INFO("Second output wrt GPlan, last out %f, tput %f", last_gplan_out_ts, tput_gplan);
+		tput_gplan_arr.push_back(tput_gplan);
+                tput_gplan_sum += tput_gplan;                
+            }
+
+            if (tput_gplan_arr.size() > 2)
+            {
+                // add rxnTime
+                double rt = ros::Time::now().toSec() - ts_last_gplan_used;
+                rxntm_gplan_arr.push_back(rt);
+                rxntm_gplan_sum += rt;
+            }
+
+            ts_last_gplan_used = using_gplan_ts;
+            last_gplan_out_ts = ros::Time::now().toSec();
+        }
+        if (using_odom_ts > ts_last_odom_used)
+        {
+            lat_odom_sum += lat_odom;
+            lat_odom_arr.push_back(lat_odom);
+
+            if (last_odom_out_ts > 0.0)
+            {
+                double tput_odom = ros::Time::now().toSec() - last_odom_out_ts;
+                // ROS_INFO("Second output wrt Odom, last out %f, tput %f", last_odom_out_ts, tput_odom);
+                tput_odom_arr.push_back(tput_odom);
+                tput_odom_sum += tput_odom;
+            }
+
+            if (tput_odom_arr.size() > 2)
+            {
+                double rt = ros::Time::now().toSec() - ts_last_odom_used;
+                rxntm_odom_arr.push_back(rt);
+                rxntm_odom_sum += rt;
+            }
+
+            last_odom_out_ts = ros::Time::now().toSec();
+            ts_last_odom_used = using_odom_ts;
+        }
+        if (using_lcmp_ts > ts_last_lcmp_used)
+        {
+            lat_lcmp_sum += lat_lcmp;
+            lat_lcmp_arr.push_back(lat_lcmp);
+
+            if (last_lcmp_out_ts > 0.0)
+            {
+                double tput_lcmp = ros::Time::now().toSec() - last_lcmp_out_ts;
+                // ROS_INFO("Second output wrt Lcmp, last out %f, tput %f", last_lcmp_out_ts, tput_lcmp);
+                tput_lcmp_arr.push_back(tput_lcmp);
+                tput_lcmp_sum += tput_lcmp;
+            }
+
+            if (tput_lcmp_arr.size() > 2)
+            {
+                double rt = ros::Time::now().toSec() - ts_last_lcmp_used;
+                rxntm_lcmp_arr.push_back(rt);
+                rxntm_lcmp_sum += rt;
+            }
+
+            last_lcmp_out_ts = ros::Time::now().toSec();
+            ts_last_lcmp_used = using_lcmp_ts;
+        }
+
+        double ros_ci = ros::Time::now().toSec() - ros_start;
+        double rt_ci = (double)(clock() - rt_start)/CLOCKS_PER_SEC;
+
+        compute_rt_arr.push_back(rt_ci);
+        compute_rt_sum += rt_ci;
+
+        compute_ros_arr.push_back(ros_ci);
+        compute_ros_sum += ros_ci;
+
+        if (total_exec_count%3 == 1)
+        {
+	    std::cout << "compute ros sum : " << compute_ros_sum << std::endl;
+            print_smt(compute_ros_sum, compute_ros_arr, "Local Planner Exec ROS time");
+            print_smt(compute_rt_sum, compute_rt_arr, "Local Planner Exec Real time");
+	    print_smt(lat_lcmp_sum, lat_lcmp_arr, "Lat w.r.t. LCMP chain (exc LPlan)");
+	    print_smt(lat_gplan_sum, lat_gplan_arr, "Lat w.r.t. GPlan chain (exc LPlan)");
+	    print_smt(lat_odom_sum, lat_odom_arr, "Lat w.r.t. Odom chain (exc LPlan)");
+	    print_smt(tput_lcmp_sum, tput_lcmp_arr, "Tput w.r.t. LCMP chain");	    
+	    print_smt(tput_gplan_sum, tput_gplan_arr, "Tput w.r.t. GPLan chain");	    
+	    print_smt(tput_odom_sum, tput_odom_arr, "Tput w.r.t. Odom chain");	    
+            print_smt(rxntm_lcmp_sum, rxntm_lcmp_arr, "RxnTm for Scan-LCMp-LPlan chain");
+	    print_smt(rxntm_gplan_sum, rxntm_gplan_arr, "RxnTm for Scan-GCmp-GPlan-LPlan chain");
+	    print_smt(rxntm_odom_sum, rxntm_odom_arr, "RxnTm for Odom-LPlan chain");
+	}
+    }
+
+    void lcmpCallback(const std_msgs::Header::ConstPtr& msg)
+    {
+        last_lcmp_ts = msg->stamp.toSec();
+        total_lcmp_cb_count += 1;
+        // If not RTC, i.e. fractional. E.D. w.r.t. Lcmp
+        if (!rtc)
+            exec();
+    }
+};
+
+int main(int argc, char **argv)
+{
+    int lim = atoi(argv[1]);
+    bool rtc = (atoi(argv[2]) == 1);
+    std::string lcmp_topic = argv[3];
+    std::string odom_topic = argv[4];
+    std::string gplan_topic = argv[5];
+    ros::init(argc, argv, "dummyLPlan");
+    LocalPlanner lp(lim, rtc, lcmp_topic, odom_topic, gplan_topic);
+    ros::spin();
+    return 0;
+}
