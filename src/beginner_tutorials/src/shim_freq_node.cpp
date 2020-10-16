660964df8d15df16f26816c72a7e3d234ff86f57
Aditi <cs1140205@iitd.ac.in>
2020-10-16 00:50:11 -0500

    UPdated shim freq node to measure real pub freq

--- src/beginner_tutorials/src/shim_freq_node.cpp
+++ src/beginner_tutorials/src/shim_freq_node.cpp
@@ -2,6 +2,7 @@
 #include "std_msgs/String.h"
 #include "std_msgs/Header.h"
 #include "sensor_msgs/Image.h"
+#include <sensor_msgs/LaserScan.h>
 #include <fstream>
 #include <sstream>
 #include <cmath>
@@ -37,16 +38,24 @@ public:
     ros::Timer sensor_data_publish_thread;
     void publishLatestData(const ros::TimerEvent& event);
 
-    // for locking the data fields used both by publisher & subscriber.
-    protected: boost::mutex data_lock; 
-
     ros::NodeHandle nh;
 
+    double ros_last_recv_ts;
+    clock_t real_last_recv_ts;
+    struct timespec real_cg_last_recv_ts;
+    std::vector<double> ros_recv_deltas, real_recv_deltas, real_cg_recv_deltas;
+
+    void printVecStats(std::vector<double> v, std::string s);
+
     // ShimFreqNode();
-    ShimFreqNode(double f, std::string topic);
+    ShimFreqNode(double f, std::string sub_topic, std::string pub_topic, std::string msg_type);
+
+    // for locking the data fields used both by publisher & subscriber.
+protected: 
+    boost::mutex data_lock; 
 };
 
-void ShimFreqNode::ShimFreqNode(double f, std::string sub_topic, std::string pub_topic, std::string msg_type)
+ShimFreqNode::ShimFreqNode(double f, std::string sub_topic, std::string pub_topic, std::string msg_type)
 {
     freq = f;
 
@@ -55,18 +64,25 @@ void ShimFreqNode::ShimFreqNode(double f, std::string sub_topic, std::string pub
         sensor_sub = nh.subscribe(sub_topic, 1, &ShimFreqNode::scanDataCallback, this, ros::TransportHints().tcpNoDelay() );
         
         sensor_pub = nh.advertise<sensor_msgs::LaserScan> (pub_topic, 1);
-        ROS_WARN("SHIM Node for type scan, Subscribed to topic %s, Will publish to topic %s", topic, pub_topic);
+        ROS_WARN("SHIM Node for type scan, Subscribed to topic %s, Will publish to topic %s", sub_topic.c_str(), pub_topic.c_str());
     }
 
+    ros_last_recv_ts = 0.0;
+    real_last_recv_ts = 0.0;
+
     // start thread for publishing :
-    sensor_data_publish_thread = nh.createTimer(ros::Duration(1.0/freq), &ShimFreqNode::publishLatestData, this);
+    // sensor_data_publish_thread = nh.createTimer(ros::Duration(1.0/freq), &ShimFreqNode::publishLatestData, this);
 }
 
 void ShimFreqNode::scanDataCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     {
         boost::mutex::scoped_lock lock(data_lock);
-
+        
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        if (ros_recv_deltas.size()%20 == 7)
+            ROS_WARN("Got scan with TS %f, current clockMono time %f %f", msg->header.stamp.toSec(), ts.tv_sec, ts.tv_nsec * 1e-9);
         latest_scan.header.frame_id = msg->header.frame_id;
         latest_scan.header.seq = msg->header.seq;
         latest_scan.header.stamp = msg->header.stamp;
@@ -78,13 +94,47 @@ void ShimFreqNode::scanDataCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
         latest_scan.scan_time = msg->scan_time;
         latest_scan.range_min = msg->range_min;
         latest_scan.range_max = msg->range_max;
-        latest_scan.ranges = msg->ranges;
-        latest_scan.intensities = msg->intensities;
 
+        latest_scan.ranges.clear();
+        latest_scan.intensities.clear();
 
-        ROS_WARN("COPIED incoming msg into latest_scan. Verifying : 5th: %i %i, 111th: %i %i", latest_scan.ranges[5], msg->ranges[5], latest_scan.ranges[111], msg->ranges[111]);
+        for (int i = 0; i < msg->ranges.size(); i++)
+            latest_scan.ranges.push_back(msg->ranges[i]);
+
+        for (int i = 0; i < msg->intensities.size(); i++)
+            latest_scan.intensities.push_back(msg->intensities[i]);
+
+        if (ros_last_recv_ts > 0.0)
+        {
+            ros_recv_deltas.push_back(ros::Time::now().toSec() - ros_last_recv_ts);
+            real_recv_deltas.push_back( double(clock() - real_last_recv_ts)/ double(CLOCKS_PER_SEC) );
+            struct timespec end;
+            clock_gettime(CLOCK_MONOTONIC, &end);
+            real_cg_recv_deltas.push_back( (end.tv_sec - real_cg_last_recv_ts.tv_sec) + ((end.tv_nsec - real_cg_last_recv_ts.tv_nsec) * 1e-9) );
+        }
+
+        if (ros_recv_deltas.size()%100 == 57)
+        {
+            printVecStats(ros_recv_deltas, "ROS_Time_B/W_Recv_Scans");
+            printVecStats(real_recv_deltas, "Real_Time_B/W_Recv_Scans");
+            printVecStats(real_cg_recv_deltas, "Real_CG_Time_B/W_Recv_Scans");
+        }
+
+        
+        ros_last_recv_ts = ros::Time::now().toSec();
+        real_last_recv_ts = clock();
+        clock_gettime(CLOCK_MONOTONIC, &real_cg_last_recv_ts);
     }
+}
 
+void ShimFreqNode::printVecStats(std::vector<double> v, std::string st)
+{
+    std::sort(v.begin(), v.end());
+    if (v.size() > 0)
+    {
+        int s = v.size();
+        ROS_WARN("%s : Mean %f, Median %f, 95ile %f, 99ile %f", st.c_str(), std::accumulate(v.begin(), v.end(), 0.0)/s, v[s/2], v[(95*s)/100], v[(99*s)/100] );
+    }
 }
 
 void ShimFreqNode::publishLatestData(const ros::TimerEvent& event)
@@ -97,8 +147,9 @@ void ShimFreqNode::publishLatestData(const ros::TimerEvent& event)
     }
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "shim_freq_node");
     ShimFreqNode sfn( atof(argv[1]), argv[2], argv[3], argv[4] );
     ros::spin();
     return 0;
