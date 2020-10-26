7d364a077fceddb0c6ecee12e1d451780f73b2d1
Ubuntu <ubuntu@ip-172-31-68-124.ec2.internal>
2020-10-26 15:24:55 +0000

    Adding mc1 nav2d, roscomm

---
+++ src_mc1_oct25/navigation_2d/nav2d_karto/include/nav2d_karto/MultiMapper.h
@@ -0,0 +1,156 @@
+#ifndef MULTI_MAPPER_H
+#define MULTI_MAPPER_H
+
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/GetMap.h>
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <nav2d_msgs/LocalizedScan.h>
+#include <nav2d_localizer/SelfLocalizer.h>
+
+#include <OpenKarto/OpenKarto.h>
+
+#include <string>
+#include <map>
+
+#include <fstream>
+#include <boost/thread.hpp>
+
+#define ST_WAITING_FOR_MAP  10
+#define ST_LOCALIZING       20
+#define ST_MAPPING          30
+
+// for getting system clock time
+#include <boost/chrono/system_clocks.hpp>
+#include <boost/chrono/ceil.hpp>
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
+
+class MultiMapper
+{
+public:
+	// Constructor & Destructor
+	MultiMapper();
+	~MultiMapper();
+
+	// Public methods
+	void receiveLaserScan(const sensor_msgs::LaserScan::ConstPtr& scan);
+	void receiveLocalizedScan(const nav2d_msgs::LocalizedScan::ConstPtr& scan);
+	void receiveInitialPose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose);
+	void sendLocalizedScan(const karto::Pose2& pose);
+	void onMessage(const void* sender, karto::MapperEventArguments& args);
+	bool getMap(nav_msgs::GetMap::Request  &req, nav_msgs::GetMap::Response &res);
+	void publishLoop();
+	void publishTransform();
+	void setScanSolver(karto::ScanSolver* scanSolver);
+
+	// For measuring RT:
+
+	// TS of last scan processed by mapCB.
+	double last_scan_mapCB_processed;
+
+	// TSS of last scan used in generating the mapToOdom TF.
+	double last_scan_mapCB_tf_processed;
+
+	// For measuring Tput of subchains MapCB and MapUpdate:
+	std::vector<double> tput_map_cb, tput_map_update;
+	boost::chrono::time_point<boost::chrono::system_clock> last_map_cb_out;
+	boost::chrono::time_point<boost::chrono::system_clock> last_map_upd_out;
+	long int total_mapcb_count, total_mapupdate_count;
+
+	ros::Publisher mScanTSPublisher;
+
+	// For converting the module into hybrid ED/TD
+	// Making a separate thread for mapUpdates
+	boost::thread* map_update_thread_;
+	bool map_update_thread_shutdown_;
+	bool received_scans, processed_scans;
+	void mapUpdateLoop(double freq);
+
+	// Making a thread for mapCB as well:
+	boost::thread* map_scan_cb_thread_;
+	bool map_scan_cb_thread_shutdown_;
+	sensor_msgs::LaserScan latest_scan_recv; // this is what'll be processed by the mapScanCBLoop.
+	void mapScanCBLoop(double freq);
+	void processLatestScan();
+	double mMapScanUpdateRate; // Oct: period of scanCB thread.
+
+	boost::mutex scan_lock; // to lock usage of latest_scan_recv.
+
+	ros::Publisher map_cb_exec_info_pub, map_upd_exec_info_pub;
+
+	// Oct: making mapCB ED as well.
+	// For fractional scheduling:
+	int mDropFraction;
+
+private:
+	// Private methods
+	bool updateMap();
+	bool sendMap();
+	void setRobotPose(double x, double y, double yaw);
+	karto::LocalizedRangeScan* createFromRosMessage(const sensor_msgs::LaserScan& scan, const karto::Identifier& robot);
+
+	// Particle filter to localize within received map
+	SelfLocalizer* mSelfLocalizer;
+	
+	// Everything related to ROS
+	tf::TransformListener mTransformListener;
+	tf::TransformBroadcaster mTransformBroadcaster;
+	tf::Transform mMapToOdometry;
+	tf::Transform mOdometryOffset;
+
+	nav_msgs::OccupancyGrid mGridMap;
+
+	ros::ServiceServer mMapServer;
+	ros::Publisher mMapPublisher;
+	ros::Publisher mScanPublisher;
+	ros::Publisher mVerticesPublisher;
+	ros::Publisher mEdgesPublisher;
+	ros::Publisher mPosePublisher;
+	ros::Publisher mOtherRobotsPublisher;
+	ros::Subscriber mLaserSubscriber;
+	ros::Subscriber mScanSubscriber;
+	ros::Subscriber mInitialPoseSubscriber;
+
+	// Everything related to KARTO
+	karto::LaserRangeFinderPtr mLaser;
+	karto::SmartPointer<karto::OpenMapper> mMapper;
+	std::map<int, karto::LaserRangeFinderPtr> mOtherLasers;
+	bool mMapChanged;
+
+	// Parameters and Variables
+	int mRobotID;               // Who am I?
+	double mMapResolution;      // Resolution of published grid map.
+	double mRangeThreshold;     // Maximum range of laser sensor. (All robots MUST use the same due to Karto-Mapper!)
+	double mMaxCovariance;      // When to accept the result of the particle filter?
+	int mState;	                // What am I doing? (waiting, localizing, mapping)
+	double mMapUpdateRate;	        // Publish the map every # received updates.
+	bool mPublishPoseGraph;	    // Whether or not to publish the pose graph as marker-message.
+	int mNodesAdded;            // Number of nodes added to the pose graph.
+	int mMinMapSize;            // Minimum map size (# of nodes) needed for localization.
+	ros::WallTime mLastMapUpdate;
+
+	// Frames and Topics
+	std::string mLaserFrame;
+	std::string mRobotFrame;
+	std::string mOdometryFrame;
+	std::string mOffsetFrame;
+	std::string mMapFrame;
+	std::string mLaserTopic;
+	std::string mMapTopic;
+	std::string mMapService;
+	std::string mScanInputTopic;
+	std::string mScanOutputTopic;
+
+	std::vector<double> scan_cb_times;
+	std::vector<double> scan_cb_ts;
+
+	std::vector<double> map_update_times;
+	std::vector<double> map_update_ts;
+
+	std::vector<int> map_update_scan_count;
+};
+
+#endif
---
+++ src_mc1_oct25/navigation_2d/nav2d_operator/include/nav2d_operator/RobotOperator.h
@@ -0,0 +1,190 @@
+#ifndef OPERATOR_H
+#define OPERATOR_H
+
+#define NODE_NAME     "operator"
+#define COMMAND_TOPIC "cmd"
+#define CONTROL_TOPIC "cmd_vel"
+#define ROUTE_TOPIC   "route"
+#define PLAN_TOPIC    "desired"
+#define LUT_RESOLUTION 100
+
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <sensor_msgs/PointCloud.h>
+#include <nav2d_operator/cmd.h>
+
+#include <string>
+
+#include <mutex>
+#include <condition_variable>
+
+// for getting system clock time
+#include <boost/chrono/system_clocks.hpp>
+#include <boost/chrono/ceil.hpp>
+#include <boost/date_time/posix_time/posix_time_duration.hpp>
+
+// Helper Functions: Aditi
+void write_arrs_to_file(std::vector<double>& times, std::vector<double>& ts, std::string s);
+void write_arr_to_file(std::vector<double>& arr, std::string m);
+void print_arr(std::vector<double> m_arr, std::string m);
+
+/**
+ * @class RobotOperator
+ * @author Sebastian Kasperski
+ * @date 03/25/14
+ * @file RobotOperator.h
+ * @brief The core of a ROS node to provide purely reactive obstacle avoidance
+ * The RobotOperator is supposed to be placed between a path planner and a mobile
+ * robots hardware driver. It takes in motion commands defined in the robot's
+ * coordinate frame and outputs a Twist-Message to directly control a robot.
+ * The given command is continually checked against a local map updated from
+ * collected laser range data and corrected to follow a safe path.
+ */
+class RobotOperator
+{
+public:
+	// Default Constructor & Destructor
+	RobotOperator(std::condition_variable* cv_robot_op);
+	~RobotOperator();
+	
+	// Public Methods
+	/**
+	 * @brief Callback function to receive move commands
+	 * @param msg Command-Message
+	 * Direction [-1.0 .. 1.0]: -1(rotate left); 0(straight); 1(rotate right)
+	 * Velocity  [-1.0 .. 1.0]: -1(full speed back); 0(stop); 1(full speed ahead)
+	 * Mode: 0(Avoid obstacles); 1(Stop at obstacles)
+	 */
+	void receiveCommand(const nav2d_operator::cmd::ConstPtr& msg);
+
+	/**
+	 * @brief Generates and sends Twist-Message to Robot
+	 * This is the Operator's core function and should be called periodically
+	 */
+	void executeCommand();
+
+	// For measuring RT
+
+        // Need to store [the latest scan USED TS] in the last executeCmd, wrt each of the chains:
+        double last_scan_lcmp_ts; // TS of [latest scan in latest LC] used by last executeCommand
+        double last_scan_mapCB_navPlan_navCmd_ts; // TS of last scan used wrt S-MapCB-LC chain
+        double last_scan_mapCB_navCmd_ts;
+        double last_scan_mapCB_mapUpd_navPlan_navCmd_ts;
+
+        double last_scan_mapCB_ts;
+
+        // TS of the scan, based on which we have got the command.
+        double last_scan_mapCB_mapUpd_navPlan_navCmd_recv;
+
+        // TS of the scan, whose TF was used by navCmd.
+        double last_scan_mapCB_navCmd_recv;
+
+        // TS of scan, whose TF was used by navPlan, used by navCmd.
+        double last_scan_mapCB_navPlan_navCmd_recv;
+
+        // Need to store TS of [last new output] wrt each chain
+        double last_scan_lcmp_out, last_scan_mapCB_navPlan_navCmd_out, last_scan_mapCB_navCmd_out, last_scan_mapCB_mapUpd_navPlan_navCmd_out;
+
+        // Vector of latency for [every new output] wrt each chain
+        std::vector<double> lat_scan_lcmp_arr, lat_scan_mapCB_navCmd_arr, lat_scan_mapCB_navPlan_navCmd_arr, lat_scan_mapCB_mapUpd_navPlan_navCmd_arr;
+
+	// Vector for tput
+        std::vector<double> tput_scan_lcmp_arr, tput_scan_mapCB_navPlan_navCmd_arr, tput_scan_mapCB_navCmd_arr, tput_scan_mapCB_mapUpd_navPlan_navCmd_arr;
+
+        // Vector for RT
+        std::vector<double> rt_scan_lcmp_arr, rt_scan_mapCB_navCmd_arr, rt_scan_mapCB_navPlan_navCmd_arr, rt_scan_mapCB_mapUpd_navPlan_navCmd_arr;
+
+	double first_out_ts_scan_lcmp, first_out_ts_scan_mapCB_navCmd;
+
+	// For publishing end of exec of CC to controller:
+	ros::Publisher exec_end_cc_pub;
+
+private:
+	// Internal Methods
+	/**
+	 * @brief Calculates the distance the robot can move following the given trajectory
+	 * @param cloud PointCloud defining a trajectory
+	 * @return Nmber of free cells
+	 */
+	int calculateFreeSpace(sensor_msgs::PointCloud* cloud);
+
+	/**
+	 * @brief Calculate the action value of a given command
+	 * @param direction How to move the robot
+	 * @param velocity Only used to distinguish forward and backward motion
+	 * @param debug Publish result of evaluation functions on debug topic
+	 * @return Weighted sum of all evaluation functions
+	 * The given action is rated by 4 different evaluation functions:
+	 * Free Space: How far can the robot move following this command
+	 * Safety: How close will it get near obstacles
+	 * Conformance: How good does it follow the commanded direction
+	 * Continuity: (experimental!) How does it conform with the last issued command
+	 */
+	double evaluateAction(double direction, double velocity, bool debug = false);
+
+	/**
+	 * @brief Evaluates all possible directions with a fixed resolution
+	 * @return Best evaluated direction
+	 */
+	double findBestDirection();
+
+	/**
+	 * @brief Initializes look-up tables
+	 * This calculates the trajectories of all possible direction commands.
+	 * Must be called once before the Operator is used
+	 */
+	void initTrajTable();
+	
+	/**
+	 * @brief Get the trajectory defined by the given movement command
+	 * @param direction How to move the robot
+	 * @param velocity Only used to distinguish forward and backward motion
+	 * @return A pointer to the PointCloud defined in the robot coordinate frame
+	 */
+	inline sensor_msgs::PointCloud* getPointCloud(double direction, double velocity);
+
+	// Internal Storage
+	costmap_2d::Costmap2DROS* mLocalMap;
+	costmap_2d::Costmap2D* mCostmap;
+	double mRasterSize;
+	
+	tf::TransformListener mTfListener;
+	
+	ros::Subscriber mCommandSubscriber;
+	ros::Publisher mControlPublisher;
+	ros::Publisher mTrajectoryPublisher;
+	ros::Publisher mPlanPublisher;
+	ros::Publisher mCostPublisher;
+	
+	double mDesiredVelocity;
+	double mDesiredDirection;
+	double mCurrentVelocity;
+	double mCurrentDirection;
+	int mDriveMode;
+	
+	sensor_msgs::PointCloud* mTrajTable[(LUT_RESOLUTION * 4) + 2];
+	
+	double mMaxVelocity;
+	
+	bool mPublishRoute;
+	double mMaxFreeSpace;
+	double mSafetyDecay;
+	int mDistanceWeight;
+	int mSafetyWeight;
+	int mConformanceWeight;
+	int mContinueWeight;
+	int mEscapeWeight;
+
+	std::string mOdometryFrame;
+	std::string mRobotFrame;
+	
+	unsigned int mRecoverySteps;
+
+	std::vector<double> operator_loop_times;
+        std::vector<double> operator_loop_ts;
+
+        // For testing:
+        std::string last_odom_bl_tf;
+};
+
+#endif
