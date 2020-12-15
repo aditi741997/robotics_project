c662df7237fa105eed8e57585ebb9d98e6878f9e
Ubuntu <ubuntu@ip-172-31-63-48.ec2.internal>
2020-12-15 05:59:10 +0000

    Updating changeFreqs script to change LC,LP periods

--- change_nav2d_freqs.py
+++ change_nav2d_freqs.py
@@ -6,7 +6,10 @@ muf = float(sys.argv[3])
 ncf = float(sys.argv[4])
 npf = float(sys.argv[5])
 
-print("Got inputs: ccf %f, mcf %f, muf %f, ncf %f, npf %f"%(ccf, mcf, muf, ncf, npf))
+lcf = float(sys.argv[6])
+lpf = float(sys.argv[7])
+
+print("Got inputs: ccf %f [lcf %f, lpf %f], mcf %f, muf %f, ncf %f, npf %f"%(ccf, lcf, lpf, mcf, muf, ncf, npf))
 
 # CC will be enforced by giving the right input to the shim_freq_node.
 # Here, we set freqs for all the other NC subchains in nav2d.
@@ -15,6 +18,8 @@ pre_fname = "src/navigation_2d/nav2d_tutorials/param/"
 
 mapper_fname = pre_fname + "mapper.yaml"
 nav_fname = pre_fname + "navigator.yaml"
+ope_fname = pre_fname + "operator.yaml"
+cmp_fname = pre_fname + "costmap.yaml"
 
 with open(mapper_fname, 'r') as mf:
 	mapper_data = mf.readlines()
@@ -35,3 +40,23 @@ nav_data[22] = "replanning_period: " + str(1.0/npf) + " \n"
 
 with open(nav_fname, 'w') as nf:
 	nf.writelines(nav_data)
+
+# Local Planner freq:
+with open(ope_fname, 'r') as f:
+	ope_data = f.readlines()
+
+print("For operator, current freq: %s, new freq (LP): %f"%( ope_data[1], lpf) )
+ope_data[1] = "frequency: " + str(lpf) + "\n"
+
+with open(ope_fname, 'w') as f:
+	f.writelines(ope_data)
+
+# Local Costmap Freq:
+with open(cmp_fname, 'r') as f:
+	cmp_data = f.readlines()
+
+print("For local cmp, current freq: %s, new freq (LC): %f"%( cmp_data[2], lcf ) )
+cmp_data[2] = "update_frequency: " + str(lcf) + "\n"
+
+with open(cmp_fname, 'w') as f:
+	f.writelines(cmp_data)
