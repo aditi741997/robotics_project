516c15a5d65bdca74f23acd2c95981df49e53476
Ubuntu <ubuntu@ip-172-31-63-48.ec2.internal>
2021-01-24 23:55:17 +0000

    Updates to check Nav2d expts script

--- check_nav2d_expt.py
+++ check_nav2d_expt.py
@@ -5,24 +5,26 @@ ename = sys.argv[1]
 runs = range( int(sys.argv[2]), int(sys.argv[3])+1 )
 
 bad_runs = { -1}
+efails = {-1}
 
 # check NEG lat at shim
-for r in runs:
-	neg_ct = 0
-	neg_ratio = 0.0
-	with open("nav2d_shim_logs_"  + ename + "_run" + str(r) + ".out", 'r') as f:
-		for l in f.readlines():
-			if "NEGAT" in l:	
-				neg_ratio = float( l.split(' ')[-1][:5] )
-	with open("nav2d_shim_logs_"  + ename + "_run" + str(r) + ".err", 'r') as f:
-		for l in f.readlines():
-			if "-VE LAT" in l:
-				neg_ct += 1
-	if (neg_ratio > 0.001):
-		print("Ename: %s, run: %i, has NEG LAT ratio : %f"%(ename, r, neg_ratio) )		
-	if (neg_ct > 4):
-		print("Ename: %s, run: %i, has NEG LAT count : %f"%(ename, r, neg_ct) )
-		bad_runs.add(r)
+if "Default" not in ename:
+	for r in runs:
+		neg_ct = 0
+		neg_ratio = 0.0
+		with open("nav2d_shim_logs_"  + ename + "_run" + str(r) + ".out", 'r') as f:
+			for l in f.readlines():
+				if "NEGAT" in l:	
+					neg_ratio = float( l.split(' ')[-1][:5] )
+		with open("nav2d_shim_logs_"  + ename + "_run" + str(r) + ".err", 'r') as f:
+			for l in f.readlines():
+				if "-VE LAT" in l:
+					neg_ct += 1
+		if (neg_ratio > 0.001):
+			print("Ename: %s, run: %i, has NEG LAT ratio : %f"%(ename, r, neg_ratio) )		
+		if (neg_ct > 4):
+			print("Ename: %s, run: %i, has NEG LAT count : %f"%(ename, r, neg_ct) )
+			bad_runs.add(r)
 
 # check mMaxVel = 0.3
 for r in runs:
@@ -30,7 +32,10 @@ for r in runs:
 		for l in f.readlines():
 			if "MaxVel" in l:
 				try:
-					maxVel = float( l.split(' ')[-3][:-1] )
+					if "Default" not in ename:
+						maxVel = float( l.split(' ')[-3][:-1] )
+					else:
+						maxVel = float( l.split(' ')[-1][:5] )
 					if (maxVel != 0.3):
 						print("Ename: %s, run: %i, has WRONG maxVel: %f"% (ename, r, maxVel) )
 						bad_runs.add(r)
@@ -77,13 +82,19 @@ for r in runs:
         with open("nav2d_robot_logs_" + ename + "_run" + str(r) + ".err", 'r') as f:
 		tf_err = False
 		map_fail_ct = 0
+		efail = False
 		for l in f.readlines():
 			if "Could not get robot position" in l:
 				tf_err = True
+			if "ation fail" in l:
+				efail = True	
 			if "MAPPING F" in l:
 				map_fail_ct += 1
-		if (map_fail_ct > 1) or (tf_err):
+		if (map_fail_ct > 1) or (tf_err) or (efail):
 			bad_runs.add(r)
-			print("Ename: %s, run: %i, HAS TF error %i / Mapping failed ct: %i"%(ename, r, tf_err, map_fail_ct) )
+			print("Ename: %s, run: %i, HAS TF error %i EFail: %i / Mapping failed ct: %i"%(ename, r, tf_err, efail, map_fail_ct) )
+		if (efail):
+			efails.add(r)
 
-print("FINAL Set of bad runs: ", bad_runs)
+print("FINAL Set of bad runs: ", bad_runs, "#BadRuns: ", len(bad_runs) )
+print("#E Fails: ", len(efails), efails)
