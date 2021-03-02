08e14051e170ef4c7d2ae4c4a0126137fee1689d
Ubuntu <ubuntu@ip-172-31-63-48.ec2.internal>
2021-03-02 19:50:51 +0000

    more changes to dg

--- illixr_dag.py
+++ illixr_dag.py
@@ -10,22 +10,24 @@ debugview = 9
 
 with open("illixr_dag.txt", "w") as f:
     f.write(f"""
-N {offline_imu} 0.1 5 0 0
-N {gtsam_integrator} 0.25 5 0 0
-N {timewarp_gl} 4.0 0 10 10
-N {gldemo} 2.0 0 0 0
-N {offline_cam} 2 0 0 0
-N {open_vins} 15 0 0 0
+N {offline_imu} 0.02 5 0 0
+N {gtsam_integrator} 0.04 5 0 0
+N {timewarp_gl} 0.15 0 8.33 8.33 
+N {gldemo} 0.115 0 0 0
+N {offline_cam} 3.5 0 0 0
+N {open_vins} 15.65 0 0 0
 E {offline_imu} {gtsam_integrator} {open_vins} X
-E {open_vins} {timewarp_gl} {gldemo}  X
+E {open_vins} {gtsam_integrator} {timewarp_gl} {gldemo}  X
 E {gldemo} {timewarp_gl} X
 E {gtsam_integrator} {gldemo} {timewarp_gl} X
 E {offline_cam} {open_vins} X
-C 28 {offline_imu} {gtsam_integrator} {timewarp_gl} X
-C 400 {offline_imu} {gtsam_integrator} {gldemo} {timewarp_gl} X
-C 500 {offline_cam} {open_vins} {gtsam_integrator} {timewarp_gl} X
-C 500 {offline_imu} {open_vins} {gtsam_integrator} {timewarp_gl} X
-C 500 {offline_imu} {open_vins} {gtsam_integrator} {gldemo} {timewarp_gl} X
-C 500 {offline_cam} {open_vins} {gtsam_integrator} {gldemo} {timewarp_gl} X
+C 17 {offline_imu} {gtsam_integrator} {timewarp_gl} X
+C 22 {offline_imu} {gtsam_integrator} {gldemo} {timewarp_gl} X
+C 69 {offline_cam} {open_vins} {timewarp_gl} X
+C 77 {offline_cam} {open_vins} {gldemo} {timewarp_gl} X
+C 69 {offline_cam} {open_vins} {gtsam_integrator} {timewarp_gl} X
+C 77 {offline_cam} {open_vins} {gtsam_integrator} {gldemo} {timewarp_gl} X
 constr
 """.lstrip())
+#C 500 {offline_imu} {open_vins} {gtsam_integrator} {timewarp_gl} X
+#C 500 {offline_imu} {open_vins} {gtsam_integrator} {gldemo} {timewarp_gl} X
