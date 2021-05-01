cb5be903245bbd6ab5fe8f7f229b7538e3ac7ba4
Samuel Grayson <sam@samgrayson.me>
2021-05-01 00:07:37 -0500

    Fix stuff

--- scheduler_plugin/plugin.cpp
+++ scheduler_plugin/plugin.cpp
@@ -21,52 +21,62 @@ static size_t read_int_file(std::string path) {
 	return result;
 }
 
+static void copy(std::string inp, std::string out) {
+	auto inp_file = std::ifstream {inp, std::ios::binary};
+	auto out_file = std::ofstream {out, std::ios::binary};
+	if (inp_file.fail()) { std::cerr << "Could not open: " << inp << std::endl; }
+	if (out_file.fail()) { std::cerr << "Could not open: " << out << std::endl; }
+	out_file << inp_file.rdbuf();
+	if (inp_file.fail()) { std::cerr << "Could not read: " << inp << std::endl; }
+	if (out_file.fail()) { std::cerr << "Could not write: " << out << std::endl; }
+}
+
 // Inherit from `plugin` if you don't need the threadloop
 class scheduler_plugin : public plugin, public DAGControllerFE {
 public:
     scheduler_plugin(std::string name_, phonebook* pb_)
         : plugin{name_, pb_}
-		, cpu_freq{read_int_file("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq") / 1000}
-		, root{std::string{"../robotics_project/"}}
-		, fc{read_int_file(root + "illixr_fc_" + std::to_string(cpu_freq) + ".txt")}
-		, controller{std::make_unique<DAGControllerBE>(root + "illixr_dag_" + std::to_string(cpu_freq) + ".txt", this, false, "no", "no", 1, fc, 3, 4, 5, 6, 7, 1)}
 		, sb{pb->lookup_impl<switchboard>()}
 	{
-		if (!is_scheduler()) { abort(); }
-		std::cout << "Hello world " << controller->node_dag_mc.name_id_map.size() << "\n";
-		for (const auto& pair : controller->node_dag_mc.name_id_map) {
-			std::string name = pair.first;
-			std::cerr << "Creating entry for " << pair.first << ": " << pair.second << "\n";
-			sb->schedule<thread_info>(
-				id,
-				name + "_thread_id",
-				[this, name](switchboard::ptr<const thread_info> event, size_t) {
-					std::lock_guard<std::mutex> lock{mutex};
-					std::cerr << "thread_id for " << event->name << " is " << event->pid << std::endl;
-					controller->recv_node_info(event->name, event->pid, ::getpid());
-				}
-			);
-			sb->schedule<switchboard::event_wrapper<bool>>(
-				id,
-				name + "_completion",
-				[this, name](switchboard::ptr<const switchboard::event_wrapper<bool>>, size_t event) {
-					// controller->update_ci(name, ci);
-					if (name == controller->get_last_node_cc_name()) {
-						controller->recv_critical_exec_end();
-					} else {
-					controller->notify_node_exec_end(name);
+		if (is_scheduler()) {
+			size_t fc = std::stoi(std::getenv("ILLIXR_SCHEDULER_FC"));
+			auto dag_file = std::string{std::getenv("ILLIXR_SCHEDULER_CONFIG")};
+			controller = std::make_unique<DAGControllerBE>(dag_file, this, false, "no", "no", 1, fc, 3, 4, 5, 6, 7, 1);
+			for (const auto& pair : controller->node_dag_mc.name_id_map) {
+				std::string name = pair.first;
+				sb->schedule<thread_info>(
+					id,
+					name + "_thread_id",
+					[this, name](switchboard::ptr<const thread_info> event, size_t) {
+						std::lock_guard<std::mutex> lock{mutex};
+						std::cerr << "thread_id for " << event->name << " is " << event->pid << std::endl;
+						controller->recv_node_info(event->name, event->pid, ::getpid());
 					}
-				}
-			);
-			triggers.insert(std::make_pair<std::string, switchboard::writer<switchboard::event_wrapper<bool>>>(
-				std::move(name),
-				std::move(sb->get_writer<switchboard::event_wrapper<bool>>(name + "_trigger"))
-			));
+				);
+				sb->schedule<switchboard::event_wrapper<bool>>(
+					id,
+					name + "_completion",
+					[this, name](switchboard::ptr<const switchboard::event_wrapper<bool>>, size_t event) {
+						// controller->update_ci(name, ci);
+						if (name == controller->get_last_node_cc_name()) {
+							controller->recv_critical_exec_end();
+						} else {
+							controller->notify_node_exec_end(name);
+						}
+					}
+				);
+				triggers.insert(std::make_pair<std::string, switchboard::writer<switchboard::event_wrapper<bool>>>(
+					std::move(name),
+					std::move(sb->get_writer<switchboard::event_wrapper<bool>>(name + "_trigger"))
+				));
+			}
 		}
 	}
 
 	virtual void start2() override {
-		controller->start();
+		if (controller) {
+			controller->start();
+		}
 	}
 
 	virtual void trigger_node(std::string name, bool reset) {
@@ -76,7 +86,9 @@ public:
 	}
 
 	virtual void stop() override {
-		controller.reset();
+		if (controller) {
+			controller.reset();
+		}
 		plugin::stop();
 		// calls controller's destructor
 	}
